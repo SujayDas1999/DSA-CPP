2b7e6fce3238047a4433a52142acc24355c06cc1
Sujay Das <dasujoy1999@gmail.com>
2022-08-23 18:48:38 +0530

    Initial Commit - 1

--- LinkedList/LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -52,7 +52,7 @@ void recdisplay(struct Node* p)
     }
 }
 
-void count(struct Node* head)
+int count(struct Node* head)
 {
     struct Node* curr = head;
     int count = 0;
@@ -62,7 +62,8 @@ void count(struct Node* head)
         curr = curr->next;
     }
 
-    std::cout << "Count is " << count << "  ";
+    //std::cout << "Count is " << count << "  ";
+    return count;
 }
 
 int sum(struct Node* head)
@@ -80,7 +81,7 @@ int sum(struct Node* head)
     return sum;
 }
 
-Node* maxNode(struct Node* head)
+struct Node* maxNode(struct Node* head)
 {
     struct Node* curr = head, * ele = head;
     
@@ -100,19 +101,190 @@ Node* maxNode(struct Node* head)
     return ele;
 }
 
+bool search(struct Node* head, int key)
+{
+    struct Node* curr = head;
+    while (curr != NULL)
+    {
+        if (curr->data == key)
+        {
+            return true;
+        }
+
+        curr = curr->next;
+    }
+
+    return false;
+}
+
+void insert(struct Node* head, int pos, int data)
+{
+    struct Node* t, * p, *curr = head;
+    if (pos < 0 || pos > count(head)) return;
+
+    t = (struct Node*)malloc(sizeof(struct Node));
+    t->data = data;
+    t->next = NULL;
+
+    if (pos == 0)
+    {
+    
+        t->next = head;
+        head = t;
+        curr = t;
+    }
+    else 
+    {
+        int i = 1;
+        while (i != pos)
+        {
+            head = head->next;
+            i++;
+            //t = t->next;
+        }
+
+        t->next = head->next;
+        head->next = t;
+        head = t;
+    }
+
+    display(curr);
+}
+
+void createInsert(int a[], int count)
+{
+    struct Node* t, * p=head;
+    int pos = 0;
+
+    while (pos < count)
+    {
+        t = (struct Node*)malloc(sizeof(struct Node));
+        //t->next = NULL;
+        if (pos == 0)
+        {
+            t->data = a[pos];
+            t->next = head;
+            head = t;
+            p = head;
+        }
+        else 
+        {
+
+            t->data = a[pos];
+            t->next = NULL;
+            p->next = t;
+            p = t;
+        }
+
+        pos++;
+    }
+   
+}
+
+void insertingInSortedLL(struct Node* p, int ele)
+{
+    if (count(head) == 0) insert(head, 0, ele);
+    else 
+    {
+        struct Node* curr = head, *prev=head;
+
+       while (curr != NULL)
+       {
+           if (curr->data > ele) break;
+
+           prev = curr;
+           curr = curr->next;
+      }
+       
+       struct Node* t;
+
+       t = (struct Node*)malloc(sizeof(struct Node));
+       t->data = ele;
+       t->next = NULL;
+
+       if (curr == head)
+       {
+           t->next = head;
+           head = t;
+       }
+
+       else 
+       {
+
+           t->next = curr;
+           prev->next = t;
+       }
+      
+
+
+
+        //display(head);
+    }
+}
+
+void deleteNode(struct Node* head, int ele)
+{
+    if (head == NULL) return;
+
+    struct Node* curr = head, *prev = head;
+    
+    while (curr != NULL)
+    {
+        if (curr->data == ele) break;
+
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if (curr == head)
+    {
+        head = curr->next;
+        curr->next = NULL;
+    }
+    else 
+    {
+        prev->next = curr->next;
+        curr->next = NULL;
+    }
+    
+
+    display(head);
+
+}
+
 int main()
 {
     int a[] = { 3,5,7,10,15 };
 
-    create(a, 5);
-   // display(first);
-    recdisplay(head);
-    count(head);
-
-    std::cout << "Sum is " << sum(head) << "\n";
-
-    std::cout << "Max is " << maxNode(head)->data << " ";
+    createInsert(a, 5);
+    insertingInSortedLL(head, 4);
+    deleteNode(head,4);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
